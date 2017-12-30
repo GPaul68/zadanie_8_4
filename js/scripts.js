da1d699c3d250c43976eec6e25cd67086ecba015
Paweł Galeja <31400955+GPaul68@users.noreply.github.com>
2017-12-30 16:53:28 +0100

    Add comment line with alert method

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,8 @@ var name = prompt('Enter your name');
 //The prompt function returns the value entered in the window.
 
 alert('Hello, ' + name);
-//Displays the value in the alert window.
+/*I do not understand. I already have a commented line with an alert command :)
+Displays the value in the alert window*/
 
 console.log('Hello, ' + name);
 /* Displays the value on the console tab.*/
